a366ec1632f745c2e7e9f81279d137db216bd46d
Guupys <98361451+guupys@users.noreply.github.com>
2022-09-08 14:37:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,41 @@
 
 from setuptools import setup, find_packages
 
-setup(
-    name='test',
-    version='1.0',
+base_packages = [
+    "numpy>=1.20.0",
+    "pandas>=1.1.5",
+    "tqdm>=4.41.1",
+]
 
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+use_packages = [
+    "tensorflow",
+    "tensorflow_hub",
+    "tensorflow_text"
+]
+
+packages = base_packages + use_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="time-analysis",
+    packages=find_packages(exclude=["notebooks", "docs"]),
+    version="0.1.0",
+    author="guupys",
+    author_email="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/guupys/Time-Analysis/",
+    keywords="time series",
+    classifiers=[
+        "Programming Language :: Python",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=base_packages,
+    #extras_require={},
+    python_requires='>=3.8',
 )
